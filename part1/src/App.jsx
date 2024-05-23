349bfbd18325c4ef4d574ebc909545cff41d22a7
programador13 <programador13@tecnologicocomfenalco.edu.co>
2024-05-23 13:53:23 -0500

    part 1 1.6

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,74 +1,32 @@
-const Header = (props) => {
-  console.log(props);
-  return <h1>{props.course}</h1>;
-};
-
-const Part = (props) => {
-  return (
-    <div>
-      {props.part} {props.exercises}
-    </div>
-  );
-};
-const Content = (props) => {
-  return (
-    <div>
-      <Part
-        part={props.parts[0].name}
-        exercises={props.parts[0].exercises}
-      ></Part>
-      <Part
-        part={props.parts[1].name}
-        exercises={props.parts[1].exercises}
-      ></Part>
-      <Part
-        part={props.parts[2].name}
-        exercises={props.parts[2].exercises}
-      ></Part>
-    </div>
-  );
-};
-
-const Total = (props) => {
-  return (
-    <div>
-      Number of exercises{" "}
-      {props.parts[0].exercises +
-        props.parts[1].exercises +
-        props.parts[2].exercises}
-    </div>
-  );
-};
+import { useState } from "react";
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
-      <Header course={course.name}></Header>
+      <h1>Give feedback</h1>
+      <button  onClick={() => setGood(1 + good)}> Good</button>
+      <button   onClick={() => setNeutral(1 + neutral)}> Neutral</button>
+      <button onClick={() => setBad(1 + bad)}> Bad</button>
+      <br />
+      <h1>Statistis</h1>
+      <br />
+      <h4>count good</h4>
+      {good}
+      <br />
+      <h4>count neutral</h4>
 
-      <Content parts={course.parts}></Content>
+      {neutral}
+      <br />
+      <h4>count bad</h4>
 
-      <Total parts={course.parts}></Total>
+      {bad}
     </div>
   );
 };
+
 export default App;
