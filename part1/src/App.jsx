b0db9c6e87eb35a2bdab3df674b8f48c0e868138
programador13 <programador13@tecnologicocomfenalco.edu.co>
2024-05-23 14:17:36 -0500

    part 1 1.7

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -5,13 +5,23 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  
+
+  const total = good + neutral + bad;
+  const average = total === 0 ? 0 : (good-bad)/total;
+  const positive =total ===0 ?0 : (good/total);
+
+  const handleGoodClick = () => setGood(good + 1);
+  const handleNeutralClick = () => setNeutral(neutral + 1);
+  const handleBadClick = () => setBad(bad + 1);
+
 
   return (
     <div>
       <h1>Give feedback</h1>
-      <button  onClick={() => setGood(1 + good)}> Good</button>
-      <button   onClick={() => setNeutral(1 + neutral)}> Neutral</button>
-      <button onClick={() => setBad(1 + bad)}> Bad</button>
+      <button onClick={handleGoodClick}> Good</button>
+      <button onClick={handleNeutralClick}> Neutral</button>
+      <button onClick={handleBadClick}> Bad</button>
       <br />
       <h1>Statistis</h1>
       <br />
@@ -25,6 +35,17 @@ const App = () => {
       <h4>count bad</h4>
 
       {bad}
+
+    <br/>
+    All
+    <br />
+
+    { total}
+    <h4> Averague total</h4>
+
+      {average}
+    <h4>Positive All</h4>
+      {positive} %
     </div>
   );
 };
