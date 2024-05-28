0db62e3e3bb645488a3da664e496e0afeeffcca6
programador13 <programador13@tecnologicocomfenalco.edu.co>
2024-05-28 15:02:07 -0500

    part 2.2 and 2.3 (waching video youtube)

--- part1/src/components/Course.jsx
+++ part1/src/components/Course.jsx
@@ -1,16 +1,21 @@
-const Course =({course}) => {
-    return (
-        <div>
-            <h1> {course.name} </h1>
-            <h4>Content</h4>
-            <ul>
-                {course.parts.map((part) => 
-                <li key={part.id}> {part.name} and <b>{part.exercises}</b> </li>)}
-            </ul>
+const Course = ({ course }) => {
+  const total = course.parts.reduce((suma, part) => suma+ part.exercises,0);
+  return (
+    <div>
+      <h1> {course.name} </h1>
+      <h4>Content</h4>
+      <ul>
+        {course.parts.map((part) => (
+          <li key={part.id}>
+            {" "}
+            {part.name} and <b>{part.exercises}</b>{" "}
+          </li>
+        ))}
+      </ul>
 
-            
-        </div>
-    )
-}
+      <p> Total of <b>{total}</b> exercises</p>
+    </div>
+  );
+};
 
 export default Course;
